efbe6b5207856cd5af6f2e8032ffed15d076cdc0
Erik Lönroth <erik.lonroth@gmail.com>
2018-11-21 21:58:00 +0100

    More to make DBD work (#2)
    * Removed v2 suffix
    
    * Fixed so munge-key will update on nodes when changed in dbd. And some small bugfixes.
    
    * Added DBD support in controller.
    Added support for cluster name change that will affect nodes.
    Added support for requesting cluster name in DBD.
    
    * Changed endpoint name in interface slurm-dbd from slurm-dbd to
    slurm-dbd-consumer.
    
    * Added SelectType and SelectTypeParameters to config settings

--- src/reactive/slurm_controller.py
+++ src/reactive/slurm_controller.py
@@ -1,3 +1,6 @@
+import os
+import time
+#
 import copy
 import socket
 import charms.leadership as leadership
@@ -8,6 +11,7 @@ import charmhelpers.core.host as host
 import charms.reactive.relations as relations
 import charms.slurm.helpers as helpers
 import charms.slurm.controller as controller
+from charms.reactive import endpoint_from_flag
 
 
 flags.register_trigger(when='munge.configured',
@@ -77,7 +81,8 @@ def handle_ha(ha_endpoint):
                    'endpoint.slurm-controller-ha.departed',
                    'config.changed',
                    'slurm-controller.reconfigure',
-                   'slurm-controller.munge_updated')
+                   'slurm-controller.munge_updated',
+                   'slurm.dbd_host_updated')
 @reactive.when('leadership.set.active_controller')
 def configure_controller(*args):
     ''' A controller is only configured after leader election is
@@ -133,6 +138,23 @@ def configure_controller(*args):
     else:
         peer_data = None
 
+    # If we have a DBD relation, extract endpoint data and configure DBD setup
+    # directly, regardless if the clustername gets accepted in the DBD or not
+    if flags.is_flag_set('endpoint.slurm-dbd-consumer.joined') and leadership.leader_get('dbd_host'):
+        dbd_host = leadership.leader_get('dbd_host')
+        controller_conf.update({
+            'dbd_host': leadership.leader_get('dbd_host'),
+            'dbd_port': leadership.leader_get('dbd_port'),
+            'dbd_ipaddr': leadership.leader_get('dbd_ipaddr')
+        })
+
+    # In case we are here due to DBD join or charm config change, announce this to the nodes
+    # by changing the value of slurm_config_updated
+    if flags.is_flag_set('slurm.dbd_host_updated') or flags.is_flag_set('config.changed'):
+        ts = time.time()
+        hookenv.log('slurm.conf on controller was updated on %s, annoucing to nodes' % ts)
+        controller_conf.update({ 'slurm_config_updated': ts })
+
     # a controller service is configurable if it is an active controller
     # or a backup controller that knows about an active controller
     is_configurable = is_active or (not is_active and peer_data)
@@ -176,3 +198,46 @@ def configure_controller(*args):
 @reactive.when('slurm-controller.configured')
 def controller_ready(cluster):
     hookenv.status_set('active', 'Ready')
+
+@reactive.when('endpoint.slurm-dbd-consumer.joined')
+#@reactive.when('endpoint.slurm-dbd-consumer.changed')
+@reactive.when_not('slurm-controller.dbdname-requested')
+def send_clustername():
+    clustername = hookenv.config().get('clustername')
+    hookenv.log("ready to send %s on endpoint" % clustername)
+    endpoint = endpoint_from_flag('endpoint.slurm-dbd-consumer.joined')
+    endpoint.configure_dbd(clustername)
+    flags.set_flag('slurm-controller.dbdname-requested')
+    # clear the changed flag on endpoint, or clustername will be requested
+    # on every hook run
+    flags.clear_flag('endpoint.slurm-dbd-consumer.changed')
+
+@reactive.when('config.changed.clustername')
+def change_clustername():
+    new_clustername = hookenv.config().get('clustername')
+    hookenv.log("detected charm config cluster name change to %s" % new_clustername)
+    # will this be a race condition with configure_controller()?
+    if os.path.exists('/var/spool/slurm.state/clustername'):
+        hookenv.log("change_clustername(): removing /var/spool/slurm.state/clustername")
+        os.remove('/var/spool/slurm.state/clustername')
+    flags.clear_flag('slurm-controller.dbdname-requested')
+    flags.clear_flag('slurm-controller.dbdname-accepted')
+    # May be unnecessary at the moment, but...
+    flags.clear_flag('config.changed.clustername')
+
+@reactive.when('endpoint.slurm-dbd-consumer.dbd_host_updated')
+@reactive.when('leadership.is_leader')
+def consume_dbd_host_change(dbd_consumer):
+    # TODO: Tuples are better?
+    dbd_host = dbd_consumer.dbd_host
+    dbd_port = dbd_consumer.dbd_port
+    dbd_ipaddr = dbd_consumer.dbd_ipaddr
+    if dbd_host:
+        leadership.leader_set(dbd_host=dbd_host)
+    if dbd_port:
+        leadership.leader_set(dbd_port=dbd_port)
+    if dbd_ipaddr:
+        leadership.leader_set(dbd_ipaddr=dbd_ipaddr)
+    flags.clear_flag('endpoint.slurm-dbd-consumer.dbd_host_updated')
+    # Announce to configure_controller that the nodes need new information
+    flags.set_flag('slurm.dbd_host_updated')
